db0db825a9514dc3a31b8fe774b3f294439e6d4c
Jablan <joshuaablan@gmail.com>
2019-07-24 09:40:37 -0700

    change css for DOM manipulation

--- js/app.js
+++ js/app.js
@@ -1,26 +1,28 @@
 const btn = document.getElementById("btn");
-const student = document.getElementById("student-name")
-const restartBtn = document.getElementById("restart-btn")
+const student = document.getElementById("student-name");
+const restartBtn = document.getElementById("restart-btn");
+const message = document.getElementById("message");
 
 
-const students = ["Dan", "Dave", "Leo", "Marques", "Muhammed", "Nick", "Perice", "Timothy", "Victor", "Ziyaad", "Amandeep", "Rick"]
+const students = ["Dan", "Dave", "Leo", "Marques", "Muhammed", "Nick", "Perice", "Timothy", "Victor", "Ziyaad", "Amandeep", "Rick"];
 
 const picked = [];
 
 btn.addEventListener("click", () => {
     randomIT();
-})
+});
 restartBtn.addEventListener("click", () => {
     resetNames();
-})
+});
 
 
 const restart = () => {
     if (students.length === 0) {
-        restartBtn.style.display = "block";
+        restartBtn.style.visibility = "visible";
+        message.style.visibility = "visible";
     }
     
-}
+};
 const randomIT = () => {
     let ranNUM = Math.floor(Math.random(1)*students.length)
     picked.unshift(students[ranNUM])
@@ -34,8 +36,11 @@ const resetNames = () => {
         students.push(s)
     })
     picked.splice(0, picked.length)
-    restartBtn.style.display = "none";
-}
+    restartBtn.style.visibility = "hidden";
+    message.style.visibility = "hidden";
+    student.innerHTML = "???"
+
+};
 
 
 

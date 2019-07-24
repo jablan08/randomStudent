d1178bc360456c2aa51055b52ea575a448ce1151
Jablan <joshuaablan@gmail.com>
2019-07-24 09:33:41 -0700

    add resetNames function to reset the arrays

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const btn = document.getElementById("btn");
 const student = document.getElementById("student-name")
+const restartbtn = document.getElementById("restart-btn")
 
 
 const students = ["Dan", "Dave", "Leo", "Marques", "Muhammed", "Nick", "Perice", "Timothy", "Victor", "Ziyaad", "Amandeep", "Rick"]
@@ -9,16 +10,32 @@ const picked = [];
 btn.addEventListener("click", () => {
     randomIT();
 })
-const randomIT = () => {
-    let ranNUM = Math.floor(Math.random(1)*students.length)
-    picked.unshift(students.splice(ranNUM, 1))
-    student.innerHTML = picked[0]
-};
+restartbtn.addEventListener("click", () => {
+    resetNames();
+})
+
+
 const restart = () => {
-    if (students.length === false) {
-        btn.style.display = "none";
+    if (students.length === 0) {
+        restartbtn.style.display = "block";
     }
     
+}
+const randomIT = () => {
+    let ranNUM = Math.floor(Math.random(1)*students.length)
+    picked.unshift(students[ranNUM])
+    students.splice(ranNUM, 1)
+    student.innerHTML = picked[0]
+    restart();
+};
+
+const resetNames = () => {
+    picked.forEach((s)=>{
+        students.push(s)
+    })
+    picked.splice(0, picked.length)
+    restartbtn.style.display = "none";
+
 }
 
 
